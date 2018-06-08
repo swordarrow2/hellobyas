ec8caf312cb80636711c89f8ab53d9f385c601c9
swordarrow2 <2856986197@qq.com>
2018-06-08 14:53:45 +0800

    qr

---
+++ app/src/main/assets/QRCode-Android-master/app/src/main/java/io/github/xudaojie/qrcode/SimpleCaptureActivity.java
@@ -0,0 +1,55 @@
+package io.github.xudaojie.qrcode;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import io.github.xudaojie.qrcodelib.CaptureActivity;
+import android.content.*;
+
+/**
+ * Created by xdj on 16/9/17.
+ */
+
+public class SimpleCaptureActivity extends CaptureActivity {
+    protected Activity mActivity = this;
+
+    private AlertDialog mDialog;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        mActivity = this;
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void handleResult(String resultString) {
+        if (TextUtils.isEmpty(resultString)) {
+            Toast.makeText(mActivity, io.github.xudaojie.qrcodelib.R.string.scan_failed, Toast.LENGTH_SHORT).show();
+            restartPreview();
+        } else {
+            if (mDialog == null) {
+				ClipboardManager clipManager = (ClipboardManager) this.getSystemService(Context.CLIPBOARD_SERVICE);
+				ClipData clip = ClipData.newPlainText("结果:",resultString);
+				clipManager.setPrimaryClip(clip);
+                mDialog = new AlertDialog.Builder(mActivity)
+                        .setMessage(resultString)
+                        .setPositiveButton("确定", null)
+                        .create();
+                mDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        restartPreview();
+                    }
+                });
+            }
+            if (!mDialog.isShowing()) {
+                mDialog.setMessage(resultString);
+                mDialog.show();
+            }
+        }
+    }
+}
--- app/src/main/java/com/meng/mbrowser/tools/xmlParser.java
+++ app/src/main/java/com/meng/mbrowser/tools/xmlParser.java
@@ -1,64 +1,62 @@
 package com.meng.mbrowser.tools;
 
 import android.content.res.*;
-import android.util.Log;
-
+import android.util.*;
 import java.io.*;
-
 import org.xmlpull.v1.*;
 
-public class xmlParser {
+public class xmlParser{
     String[] values;
     boolean parsered = false;
     String xmlPath;
     String tagName;
 
-    public xmlParser(String path, String tag) {
-        xmlPath = path;
-        tagName = tag;
+    public xmlParser(String path,String tag){
+        xmlPath=path;
+        tagName=tag;
     }
 
-    public int getXmlLength() {
+    public int getXmlLength(){
         return parse(false);
     }
 
-    public String[] parseXml() {
-        values = new String[getXmlLength() + 1];
+    public String[] parseXml(){
+        values=new String[getXmlLength()+1];
         parse(true);
         return values;
     }
 
-    public boolean isParsedSeccuss() {
+    public boolean isParsedSeccuss(){
         return parsered;
     }
 
 
-    private int parse(boolean b) {
-        try {
+    private int parse(boolean b){
+        try{
             int len = 0;
             File xmlFile = new File(xmlPath);
             InputStream is = new FileInputStream(xmlFile);
             XmlPullParserFactory fac = XmlPullParserFactory.newInstance();
             XmlPullParser xrp = fac.newPullParser();
-            xrp.setInput(is, "utf-8");
-            while (xrp.getEventType() != XmlResourceParser.END_DOCUMENT) {
-                if (xrp.getEventType() == XmlResourceParser.START_TAG) {
-                    if (xrp.getName().equals(tagName)) {
-                        if (b) {
-                            values[len] = xrp.getAttributeValue(0);
+            xrp.setInput(is,"utf-8");
+            while(xrp.getEventType()!=XmlResourceParser.END_DOCUMENT){
+                if(xrp.getEventType()==XmlResourceParser.START_TAG){
+                    if(xrp.getName().equals(tagName)){
+                        if(b){
+                            values[len]=xrp.getAttributeValue(0);
                         }
                         len++;
                     }
                 }
                 xrp.next();
             }
-            if (b) {
-                values[len] = "清除所有条目";
+            if(b){
+                values[len]="清除所有条目";
             }
             return len;
-        } catch (Exception e) {
-            Log.e(getClass().getName(), e.toString());
-            return 0;
+        }catch(Exception e){
+            Log.e(getClass().getName(),e.toString());
+            return -1;
         }
     }
 
